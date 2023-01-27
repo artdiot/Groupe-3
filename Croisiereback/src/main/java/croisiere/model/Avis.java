db1313dce4e3386801a4c62528db42e203a920b9
drakkhou <florian.duche@gmail.com>
2023-01-27 14:28:23 +0100

    commit avis

--- Croisiereback/src/main/java/croisiere/model/Avis.java
+++ Croisiereback/src/main/java/croisiere/model/Avis.java
@@ -2,4 +2,47 @@ package croisiere.model;
 
 public class Avis {
 
+	private Integer id;
+	private int note;
+	private String commentaire;
+	private Client client;
+	private Voyage voyage;
+	
+	
+
+	public Avis() {
+		super();
+	}
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public int getNote() {
+		return note;
+	}
+	public void setNote(int note) {
+		this.note = note;
+	}
+	public String getCommentaire() {
+		return commentaire;
+	}
+	public void setCommentaire(String commentaire) {
+		this.commentaire = commentaire;
+	}
+	public Client getClient() {
+		return client;
+	}
+	public void setClient(Client client) {
+		this.client = client;
+	}
+	public Voyage getVoyage() {
+		return voyage;
+	}
+	public void setVoyage(Voyage voyage) {
+		this.voyage = voyage;
+	}
+	
+	
 }
