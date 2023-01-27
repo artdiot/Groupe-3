4ebdf7f640bd413ad9297fd8d1a08b0878ae9d8e
drakkhou <florian.duche@gmail.com>
2023-01-27 14:37:04 +0100

    jpa Passager

--- Croisiereback/src/main/java/croisiere/model/Passager.java
+++ Croisiereback/src/main/java/croisiere/model/Passager.java
@@ -2,12 +2,28 @@ package croisiere.model;
 
 import java.time.LocalDate;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.persistence.Transient;
+
+@Entity
+@Table(name ="cursus")
 public class Passager {
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
+	@Column (name="surname", length = 100)
 	private String nom;
+	@Column (name="firstname", length = 100)
 	private String prenom;
+	@Column(name = "birthdate", length = 10)
 	private LocalDate dateNaissance;
+	@Transient
 	private Reservation reservation;
 	
 	public Passager() {
