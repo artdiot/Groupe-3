6ed34be1057fca245ae23beda753783066038c53
Arthur Diot <arthur.diot@gmail.com>
2023-01-27 17:16:31 +0100

    VoyageRepositoryjpa

--- Croisiereback/src/main/java/croisiere/context/Application.java
+++ Croisiereback/src/main/java/croisiere/context/Application.java
@@ -4,13 +4,19 @@ import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
 
 import croisiere.repository.ICompteRepository;
+import croisiere.repository.IPlaneteRepository;
+import croisiere.repository.IVoyageRepository;
 import croisiere.repository.jpa.CompteRepositoryjpa;
+import croisiere.repository.jpa.PlaneteRepositoryjpa;
+import croisiere.repository.jpa.VoyageRepositoryjpa;
 
 public class Application {
 	private static Application instance = null;
 
 	private final EntityManagerFactory emf = Persistence.createEntityManagerFactory("croisiere");
 	private final ICompteRepository compteRepo = new CompteRepositoryjpa();
+	private final IVoyageRepository voyageRepo = new VoyageRepositoryjpa();
+	private final IPlaneteRepository planeteRepo = new PlaneteRepositoryjpa();
 
 	private Application() {
 	}
@@ -33,6 +39,14 @@ public class Application {
 
 	public static void setInstance(Application instance) {
 		Application.instance = instance;
+	}
+
+	public IVoyageRepository getVoyageRepo() {
+		return voyageRepo;
+	}
+
+	public IPlaneteRepository getPlaneteRepo() {
+		return planeteRepo;
 	}
 	
 	
--- Croisiereback/src/main/java/croisiere/repository/jpa/VoyageRepositoryjpa.java
+++ Croisiereback/src/main/java/croisiere/repository/jpa/VoyageRepositoryjpa.java
@@ -1,7 +1,13 @@
 package croisiere.repository.jpa;
 
+import java.util.ArrayList;
 import java.util.List;
 
+import javax.persistence.EntityManager;
+import javax.persistence.EntityTransaction;
+import javax.persistence.TypedQuery;
+
+import croisiere.context.Application;
 import croisiere.model.Voyage;
 import croisiere.repository.IVoyageRepository;
 
@@ -9,32 +15,144 @@ public class VoyageRepositoryjpa implements IVoyageRepository {
 
 	@Override
 	public List<Voyage> findAll() {
-		// TODO Auto-generated method stub
-		return null;
+		List<Voyage> voyages = new ArrayList<>();
+
+		EntityManager em = null;
+		EntityTransaction tx = null;
+
+		try {
+			em = Application.getInstance().getEmf().createEntityManager();
+			tx = em.getTransaction();
+			tx.begin();
+
+			TypedQuery<Voyage> query = em.createQuery("select p from Voyage p", Voyage.class);
+
+			voyages = query.getResultList();
+
+			tx.commit();
+		} catch (Exception e) {
+			e.printStackTrace();
+			if (tx != null && tx.isActive()) {
+				tx.rollback();
+			}
+		} finally {
+			if (em != null) {
+				em.close();
+			}
+		}
+
+		return voyages;
 	}
 
 	@Override
 	public Voyage findById(Integer id) {
-		// TODO Auto-generated method stub
-		return null;
+		Voyage voyage = null;
+
+		EntityManager em = null;
+		EntityTransaction tx = null;
+
+		try {
+			em = Application.getInstance().getEmf().createEntityManager();
+			tx = em.getTransaction();
+			tx.begin();
+
+			voyage = em.find(Voyage.class, id);
+
+			tx.commit();
+		} catch (Exception e) {
+			e.printStackTrace();
+			if (tx != null && tx.isActive()) {
+				tx.rollback();
+			}
+		} finally {
+			if (em != null) {
+				em.close();
+			}
+		}
+
+		return voyage;
 	}
 
 	@Override
 	public Voyage save(Voyage o) {
-		// TODO Auto-generated method stub
-		return null;
+		EntityManager em = null;
+		EntityTransaction tx = null;
+
+		try {
+			em = Application.getInstance().getEmf().createEntityManager();
+			tx = em.getTransaction();
+			tx.begin();
+
+			o = em.merge(o);
+
+			tx.commit();
+		} catch (Exception e) {
+			e.printStackTrace();
+			if (tx != null && tx.isActive()) {
+				tx.rollback();
+			}
+		} finally {
+			if (em != null) {
+				em.close();
+			}
+		}
+
+		return o;
 	}
 
 	@Override
 	public void deleteById(Integer id) {
-		// TODO Auto-generated method stub
+		EntityManager em = null;
+		EntityTransaction tx = null;
+
+		try {
+			em = Application.getInstance().getEmf().createEntityManager();
+			tx = em.getTransaction();
+			tx.begin();
+
+			TypedQuery<Voyage> query = em.createQuery("delete from Voyage p where p.id = :id", Voyage.class);
+			query.setParameter("id", id);
+
+			query.executeUpdate();
+
+			tx.commit();
+		} catch (Exception e) {
+			e.printStackTrace();
+			if (tx != null && tx.isActive()) {
+				tx.rollback();
+			}
+		} finally {
+			if (em != null) {
+				em.close();
+			}
+		}
+		
 		
 	}
 
 	@Override
 	public void delete(Voyage o) {
-		// TODO Auto-generated method stub
-		
+		EntityManager em = null;
+		EntityTransaction tx = null;
+
+		try {
+			em = Application.getInstance().getEmf().createEntityManager();
+			tx = em.getTransaction();
+			tx.begin();
+
+			em.remove(em.merge(o));
+
+			tx.commit();
+		} catch (Exception e) {
+			e.printStackTrace();
+			if (tx != null && tx.isActive()) {
+				tx.rollback();
+			}
+		} finally {
+			if (em != null) {
+				em.close();
+			}
+		}
 	}
 
 }
