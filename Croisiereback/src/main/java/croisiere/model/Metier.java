b17fccb7f91f7b7a57449130946212621511eebd
drakkhou <florian.duche@gmail.com>
2023-01-27 14:26:19 +0100

    Classe resa

---
+++ Croisiereback/src/main/java/croisiere/model/Metier.java
@@ -0,0 +1,5 @@
+package croisiere.model;
+
+public enum Metier {
+		Maintenance,Pilotage,Sécurité;
+}
--- Croisiereback/src/main/java/croisiere/model/Passager.java
+++ Croisiereback/src/main/java/croisiere/model/Passager.java
@@ -1,5 +1,60 @@
 package croisiere.model;
 
+import java.time.LocalDate;
+
 public class Passager {
 
+	private Integer id;
+	private String nom;
+	private String prenom;
+	private LocalDate dateNaissance;
+	private Reservation reservation;
+	
+	public Passager() {
+		super();
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getNom() {
+		return nom;
+	}
+
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+
+	public String getPrenom() {
+		return prenom;
+	}
+
+	public void setPrenom(String prenom) {
+		this.prenom = prenom;
+	}
+
+	public LocalDate getDateNaissance() {
+		return dateNaissance;
+	}
+
+	public void setDateNaissance(LocalDate dateNaissance) {
+		this.dateNaissance = dateNaissance;
+	}
+
+	public Reservation getReservation() {
+		return reservation;
+	}
+
+	public void setReservation(Reservation reservation) {
+		this.reservation = reservation;
+	}
+	
+	
+	
+	
 }
