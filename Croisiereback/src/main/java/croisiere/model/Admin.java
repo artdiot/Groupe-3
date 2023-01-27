12a3f3d3cec580241d4382e236d4e6151e385ccc
Arthur Diot <arthur.diot@gmail.com>
2023-01-27 15:00:19 +0100

    jpa des comptes sans les liens

--- Croisiereback/src/main/java/croisiere/model/Admin.java
+++ Croisiereback/src/main/java/croisiere/model/Admin.java
@@ -1,5 +1,10 @@
 package croisiere.model;
 
+import javax.persistence.DiscriminatorValue;
+import javax.persistence.Entity;
+
+@Entity
+@DiscriminatorValue("admin")
 public class Admin extends Compte {
 	public Admin() {
 		super();
--- Croisiereback/src/main/java/croisiere/model/Client.java
+++ Croisiereback/src/main/java/croisiere/model/Client.java
@@ -1,9 +1,16 @@
 package croisiere.model;
 
+
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.persistence.DiscriminatorValue;
+import javax.persistence.Entity;
+import javax.persistence.Transient;
+@Entity
+@DiscriminatorValue("customer")
 public class Client extends Compte {
+	@Transient
 	private List<Reservation> reservations=new ArrayList<>();
 	
 	public Client() {
