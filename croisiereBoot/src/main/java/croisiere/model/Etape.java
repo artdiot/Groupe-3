6fc57289a89ca1e3dde6c0acc856f4595dd6cf31
Arthur Diot <arthur.diot@gmail.com>
2023-02-16 17:27:29 +0100

    controller rest étape

--- croisiereBoot/src/main/java/croisiere/model/Etape.java
+++ croisiereBoot/src/main/java/croisiere/model/Etape.java
@@ -12,29 +12,40 @@ import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.OneToOne;
 import javax.persistence.Table;
-import javax.persistence.Transient;
+import javax.persistence.Version;
+
+import com.fasterxml.jackson.annotation.JsonView;
 
 @Entity
 @Table(name = "stage")
 public class Etape {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@JsonView(Views.ViewBase.class)
 	private Integer id;
+	@Version
+	@Column
+	private Integer version;
 	@Column(name = "departure")
+	@JsonView(Views.ViewBase.class)
 	private LocalDate depart;
 	@Column(name = "arrival")
+	@JsonView(Views.ViewBase.class)
 	private LocalDate arrivee;
 	
 	@OneToOne(fetch = FetchType.LAZY)
 	@JoinColumn(name = "planet_departure_id")
+	@JsonView(Views.ViewEtape.class)
 	private Planete planeteDepart;
 	
 	@OneToOne(fetch = FetchType.LAZY)
 	@JoinColumn(name = "planet_arrival_id")
+	@JsonView(Views.ViewEtape.class)
 	private Planete planeteArrive;
 	
 	@ManyToOne
 	@JoinColumn(name = "travel_id")
+	@JsonView(Views.ViewEtape.class)
 	private Voyage voyage;
 	
 	public Integer getId() {
