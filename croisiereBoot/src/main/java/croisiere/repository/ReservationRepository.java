4357c87f2d317e446e534185650362ab9e5c04b7
Arthur Diot <arthur.diot@gmail.com>
2023-02-16 16:57:23 +0100

    reservation rest sans liens

---
+++ croisiereBoot/src/main/java/croisiere/repository/ReservationRepository.java
@@ -0,0 +1,8 @@
+package croisiere.repository;
+
+import croisiere.model.Reservation;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ReservationRepository extends JpaRepository<Reservation,Integer>{
+
+}
