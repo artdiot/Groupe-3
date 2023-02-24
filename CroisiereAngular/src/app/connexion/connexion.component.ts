6578ec9fb23c8a62851568f1dccd1356b7916923
Arthur Diot <arthur.diot@gmail.com>
2023-02-24 16:08:59 +0100

    pas fini

--- CroisiereAngular/src/app/connexion/connexion.component.ts
+++ CroisiereAngular/src/app/connexion/connexion.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthService } from '../service/auth.service';
 
 @Component({
   selector: 'app-connexion',
@@ -6,5 +8,18 @@ import { Component } from '@angular/core';
   styleUrls: ['./connexion.component.scss']
 })
 export class ConnexionComponent {
+  login: string;
+  password: string;
 
+  constructor(private authService: AuthService, private router: Router) {
+
+  }
+
+  connect(): void {
+    this.authService.connection(this.login, this.password);
+    }
+
+  isConnected(): boolean {
+    return (this.authService.checkUser()?true:false)
+  }
 }
