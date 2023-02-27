8d728fb62b9032a0a90860030156ca42aafbf4b9
RafiqSalem <rafiq.salem91@gmail.com>
2023-02-27 13:36:14 +0100

    Lien

--- CroisiereAngular/src/app/reservation/reservation.component.ts
+++ CroisiereAngular/src/app/reservation/reservation.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthService } from '../service/auth.service';
 
 @Component({
   selector: 'app-reservation',
