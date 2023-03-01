cf5afaf6a22b0834dbd181e2e48918ea03df8aea
drakkhou <florian.duche@gmail.com>
2023-03-01 15:44:48 +0100

    fix admin

--- CroisiereAngular/src/app/voyage/voyage.component.ts
+++ CroisiereAngular/src/app/voyage/voyage.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { Etape, Planete, Voyage } from '../model';
 import { EtapeService } from '../service/Etape.service';
 import { PlaneteService } from '../service/planete.service';
@@ -27,7 +27,7 @@ export class VoyageComponent {
   listeVide : Boolean = true;
   
 
-  constructor(private route: ActivatedRoute, private planeteService: PlaneteService, private etapeService: EtapeService, private voyageService: VoyageService){
+  constructor(private router : Router, private route: ActivatedRoute, private planeteService: PlaneteService, private etapeService: EtapeService, private voyageService: VoyageService){
     this.route.params.subscribe(params => {
       this.id=params["id"];
       console.log("id connexion " + this.id);
@@ -68,7 +68,7 @@ export class VoyageComponent {
     
     this.formVoyage.etapes= this.etapes;
     this.voyageService.create(this.formVoyage);    
-
+    this.router.navigate(['/accueiladmin']);
   }
 
   listPlanete():Array<Planete>{
